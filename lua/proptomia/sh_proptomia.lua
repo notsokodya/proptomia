b4d109910c8d8e66d74f18f6d43994f278fb7a9c
notsokodya <66978308+notsokodya@users.noreply.github.com>
2024-01-15 01:29:00 +0300

    added debug logs and support for my upcoming tool, edited way how players and entities being logged
    + proptomia.LogDebug(...)
    + proptomia.debug [bool]
    * player_entity -> "Kodya[STEAMID:blah blah]"
       entity -> "[5][func_door]"

--- lua/proptomia/sh_proptomia.lua
+++ lua/proptomia/sh_proptomia.lua
@@ -1,7 +1,8 @@
 proptomia = proptomia or {
     props = {},
     buddies = {},
-    convars = {}
+    convars = {},
+    debug = false -- for debug logs and etc
 }
 
 local function concat(...)
@@ -9,7 +10,15 @@ local function concat(...)
     local str = ""
 
     for k, v in next, tbl do
-        str = str .. tostring(v) .. " "
+        if isentity(v) then
+            if v:IsPlayer() then
+                str = str .. v:Name(true) .. "[" .. v:SteamID() .. "] "
+            else
+                str = str .. "[" .. v:EntIndex() .. "]" .. "[" .. v:GetClass() .. "]"
+            end
+        else
+            str = str .. tostring(v) .. " "
+        end
     end
 
     return str .. "\n"
@@ -17,14 +26,32 @@ end
 local color_gray, color_wblue, color_blue, color_yellow, color_red =
     Color(175, 175, 175), Color(150, 150, 255), Color(100, 100, 255), Color(255, 255, 100), Color(255, 100, 100)
 function proptomia.LogInfo(...)
-    MsgC(color_wblue, "Proptomia ", color_gray, "[", color_blue, " INFO ", color_gray, "]\t", color_white, concat(...))
+    local s = concat(...)
+
+    MsgC(color_wblue, "Proptomia ", color_gray, "[", color_blue, " INFO ", color_gray, "]\t", color_white, s)
+    if signalLogger then signalLogger.Info("proptomia", s) end -- support for my logger :p
 end
 function proptomia.LogWarn(...)
-    MsgC(color_wblue, "Proptomia ", color_gray, "[", color_yellow, " WARN ", color_gray, "]\t", color_white, concat(...))
+    local s = concat(...)
+
+    MsgC(color_wblue, "Proptomia ", color_gray, "[", color_yellow, " WARN ", color_gray, "]\t", color_white, s)
+    if signalLogger then signalLogger.Warn("proptomia", s) end
 end
 function proptomia.LogError(...)
-    MsgC(color_wblue, "Proptomia ", color_gray, "[", color_red, " ERROR ", color_gray, "]\t", color_white, concat(...))
+    local s = concat(...)
+
+    MsgC(color_wblue, "Proptomia ", color_gray, "[", color_red, " ERROR ", color_gray, "]\t", color_white, s)
     debug.Trace()
+
+    if signalLogger then signalLogger.Error("proptomia", s) end
+end
+function proptomia.LogDebug(...)
+    local s = concat(...)
+
+    if proptomia.debug then
+        MsgC(color_wblue, "Proptomia ", color_gray, "[", color_wblue, " DEBUG ", color_gray, "]\t", color_white, s)
+    end
+    if signalLogger then signalLogger.Debug("proptomia", s) end
 end
 
 proptomia.LogInfo("Loading files...")
